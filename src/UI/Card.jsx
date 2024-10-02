636ad136a15003138901bcda7e58b92542394ee1
Akash Biswas <akashrahul2006@gmail.com>
2024-10-02 21:32:22 +0530

    changed

--- src/UI/Card.jsx
+++ src/UI/Card.jsx
@@ -1,19 +1,39 @@
 import React from 'react';
+import { MdOutlineDraw } from 'react-icons/md';
 import Tilt from 'react-parallax-tilt';
+
 const Card = ({ icon, heading, id, description }) => {
+  // Determine if the screen size is small (e.g., less than 768px)
+  const isSmallDevice = window.innerWidth < 768;
+
   return (
-    <Tilt tiltMaxAngleX={30} tiltMaxAngleY={30} scale={1.01} transitionSpeed={0}>
-      <div className="backdrop-blur-lg shadow-[0px_0px_15px_rgba(0,0,0,0.09)] p-9 space-y-3 relative overflow-hidden border-2 border-white/30 rounded-lg cursor-pointer transition-all duration-300 hover:shadow-[0_0_25px_rgba(138,43,226,0.6)]">
-        <div className="w-24 h-24 bg-violet-500 rounded-full absolute -right-5 -top-7">
-          <p className="absolute bottom-6 left-7 text-white text-2xl">{`${id<10 ?`0${id}`:id}`}</p>
+    <>
+      {isSmallDevice ? (
+        <div
+          className="bg-white/30 backdrop-blur-lg shadow-[0px_0px_15px_rgba(0,0,0,0.09)] p-9 space-y-3 relative overflow-hidden border-2 border-white/30 rounded-lg cursor-pointer transition-all duration-300 hover:shadow-[0_0_25px_rgba(138,43,226,0.6)]"
+        >
+          <div className="w-24 h-24 bg-violet-500 rounded-full absolute -right-5 -top-7">
+            <p className="absolute bottom-6 left-7 text-white text-2xl">02</p>
+          </div>
+          {icon}
+          <h1 className="font-bold text-xl">{heading}</h1>
+          <p className="text-md text-zinc-500 leading-6">{description}</p>
         </div>
-      {icon}
-        <h1 className="font-bold text-xl">{heading}</h1>
-        <p className="text-md text-zinc-500 leading-6">
-          {description}
-        </p>
-      </div>
-    </Tilt>
+      ) : (
+        <Tilt tiltMaxAngleX={30} tiltMaxAngleY={30} scale={1.08} transitionSpeed={20}>
+          <div
+            className="bg-white/30 backdrop-blur-lg shadow-[0px_0px_15px_rgba(0,0,0,0.09)] p-9 space-y-3 relative overflow-hidden border-2 border-white/30 rounded-lg cursor-pointer transition-all duration-300 hover:shadow-[0_0_25px_rgba(138,43,226,0.6)]"
+          >
+            <div className="w-24 h-24 bg-violet-500 rounded-full absolute -right-5 -top-7">
+              <p className="absolute bottom-6 left-7 text-white text-2xl">02</p>
+            </div>
+            {icon}
+            <h1 className="font-bold text-xl">{heading}</h1>
+            <p className="text-md text-zinc-500 leading-6">{description}</p>
+          </div>
+        </Tilt>
+      )}
+    </>
   );
 };
 
